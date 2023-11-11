cf10adcbc3f809842d425866b4a50d2397fa7314
AshwiniRajashekar <ashwinirajashekar1111@gmail.com>
2023-11-11 11:15:18 +0530

    assign2

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int password;
+            Console.WriteLine("Enter Username"); 
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Password");
+            password = int.Parse(Console.ReadLine());
+            if(name == "admin" && password == 123)
+            {
+                Console.WriteLine("Enter user name : \n" + name + "\n" + "Enter password \n" + password);
+                Console.WriteLine("Login successfull");
+            }
+            else
+            {
+                Console.WriteLine("Enter user name :" + name + "Enter password" + password);
+                Console.WriteLine("Login failed");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
